52352361de5ea308470ba6159893c4f8300fc083
Bhargav <bhargavswamypm@gmail.com>
2026-06-19 00:42:42 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,27 @@ import Products from "./pages/Products";
 import Partner from "./pages/Partner";
 import Contact from "./pages/Contact";
 import Specifications from "./pages/Specifications";
-import WhatsappButton from "./components/WhatsappButton";
 import ProductDetails from "./pages/ProductDetails";
+import WhatsappButton from "./components/WhatsappButton";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/partner" element={<Partner />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/specifications" element={<Specifications />} />
-            <WhatsappButton />
-            <Route
-  path="/products/:id"
-  element={<ProductDetails />}
-/>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/partner" element={<Partner />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route
+          path="/specifications"
+          element={<Specifications />}
+        />
+      </Routes>
+
+      <WhatsappButton />
+    </>
   );
 }
 
